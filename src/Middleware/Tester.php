9a0c42de3031492d122e257e99a78e4c32931d7f
Majid Al Zariey <malzariey@gmail.com>
2025-05-10 19:02:37 +0300

    Commit

---
+++ src/Middleware/Tester.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace malzariey\ProductionDebugbar\Middleware;
+
+use App\Cookies\EnableDebugBarInProduction;
+use Closure;
+use Illuminate\Http\Request;
+use malzariey\ProductionDebugbar\ProductionDebugbar;
+use Symfony\Component\HttpFoundation\Response;
+use function App\Http\Middleware\redirect;
+
+class Tester
+{
+    public function handle(Request $request, Closure $next): Response
+    {
+        if (str_contains($request->fullUrl(),config('production-debugbar.url_key'))) {
+            return redirect('/')->withCookie(
+                ProductionDebugbar::create()
+            );
+        }
+
+        return $next($request);
+    }
+}
